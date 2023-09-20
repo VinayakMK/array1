85dde42d78fa4f01dbc443ac8fee6c6857fa9c7c
Vinayak MK <48713290+VinayakMK@users.noreply.github.com>
2023-09-20 23:53:03 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,15 @@
+/*const arr = require("readline-sync");
+
+let array = arr.questionInt("Enter size of the Array :");
+let string = arr.question("Enter Elements :");
+
+for(i=0;i<array;i++)
+
+    {
+        arr[i]=arr.question();
+        
+    }
+
+
+console.log(arr);*/
+
